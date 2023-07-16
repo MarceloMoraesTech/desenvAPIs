2e24175eaeef6f0c938cad111d861dd79783aa6e
Marcelo Moraes <stereoheart.songs@gmail.com>
2023-07-16 19:12:34 -0300

    Projeto Final (autenticação concluida)

---
+++ projeto_final/src/auth.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+
+
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+
+const GOOGLE_CLIENT_ID = '1058824386616-tl4ng82j47ht348kl2s9pr1jb4nggop8.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3333/google/callback',
+  passReqToCallback: true
+},
+
+//TODO conect with database of users (it should serach in the database if user is already registered)
+function(request, accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}
+));
+
+passport.serializeUser(function(user,done){
+  done(null, user);
+});
+
+passport.deserializeUser(function(user,done){
+  done(null, user);
+});
--- projeto_final/src/router.js
+++ projeto_final/src/router.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const deskscontroller = require('./controllers/desksController');
+const passport = require('passport');
+const session = require('express-session');
 
 const router = express.Router();
 
+require('./auth');
+
+router.use(session({secret:'cats',resave:false,saveUninitialized: false}));
+router.use(passport.initialize());
+router.use(passport.session());
+
+//manda mensagem de não autorizado caso não esteja logado
+function isLoggedIn(req,res,next){
+  req.user ? next() : res.sendStatus(401);
+}
+
 router.get('/reservas',deskscontroller.getAll);
+
 router.get('/estacoes_trabalho',deskscontroller.getAllDesks);
 
 router.post('/estacoes_trabalho',deskscontroller.criarReserva);
@@ -11,6 +25,30 @@ router.post('/estacoes_trabalho',deskscontroller.criarReserva);
 router.delete('/deletar/:id',deskscontroller.deleteReserva);
 
 router.put('/reservas/:id',deskscontroller.updateReserva);
+
+router.get('/', (req,res)=> {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+
+router.get('/auth/google', passport.authenticate('google',{scope: ['email','profile']})
+);
+
+router.get('/google/callback', passport.authenticate('google', {successRedirect:'/protected', failureRedirect:'/auth/failure'}));
+
+router.get('/protected', isLoggedIn, (req,res)=>{
+  res.send(`Hello ${req.user.displayName}`);
+});
+
+router.get('/logout',(req,res)=>{
+  req.logOut(() => {
+    req.session.destroy();
+    res.send('Goodbye!');
+  });
+});
+
+router.get('/auth/failure',(req,res)=>{
+  res.send('Something went wrong...');
+});
 
 
 module.exports = router;
